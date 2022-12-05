582fef1520bc701ac429a44894fce02a23038a26
Douglas DeVicariis <douglas@devicariis.org>
2022-12-04 21:48:51 -0500

    Added ability to get all ships from a user

--- fnar/fnar.go
+++ fnar/fnar.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/pkg/errors"
@@ -19,14 +20,19 @@ type Authentication struct {
 const BaseUrl = "https://rest.fnar.net"
 
 func Login(username string, password string) (*Authentication, error) {
+	client := &http.Client{}
 	values := map[string]string{"UserName": username, "Password": password}
 	valuesJson, err := json.Marshal(values)
 	if err != nil {
 		return nil, errors.Wrap(err, "marshaling values")
 	}
-	resp, err := http.Post(BaseUrl+"/auth/login", "application/json", bytes.NewBuffer(valuesJson))
+	req, err := http.NewRequest("POST", BaseUrl+"/auth/login", bytes.NewBuffer(valuesJson))
 	if err != nil {
-		return nil, errors.Wrap(err, "logging in")
+		return nil, errors.Wrap(err, "creating new login request")
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "sending login request")
 	}
 	defer resp.Body.Close()
 
@@ -35,7 +41,7 @@ func Login(username string, password string) (*Authentication, error) {
 		if err != nil {
 			return nil, err
 		}
-		return nil, errors.Wrap(errors.New(string(b)), "not 200")
+		return nil, errors.Wrap(errors.New(string(b)), "getting response for logging in")
 	}
 
 	body, err := io.ReadAll(resp.Body)
@@ -56,13 +62,41 @@ func Login(username string, password string) (*Authentication, error) {
 	return auth, nil
 }
 
+func (a *Authentication) GetUsername() (string, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", BaseUrl+"/auth", nil)
+	if err != nil {
+		return "", errors.Wrap(err, "creating new required")
+	}
+	req.Header.Add("Authorization", a.AuthToken)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errors.Wrap(err, "getting response")
+	}
+
+	if resp.StatusCode != 200 {
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		return "", errors.Wrap(errors.New(string(b)), strconv.Itoa(resp.StatusCode))
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "reading the response body")
+	}
+
+	return string(body), nil
+}
+
 func (a *Authentication) parse(auth map[string]interface{}) error {
 	// correct timestamp to time.Time
 	var timestamp time.Time
 	var err error
 	ts, ok := auth["Expiry"]
 	if ok {
-		timestamp, err = time.Parse("2006-01-02T15:04:05.999999", ts.(string))
+		timestamp, err = time.Parse("2006-01-02T15:04:05.999999Z", ts.(string))
 		if err != nil {
 			return err
 		}
---
+++ fnar/ship/pkg.go
@@ -0,0 +1,121 @@
+package ship
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/koo04/fnar-go/fnar"
+	"github.com/pkg/errors"
+)
+
+type Ship struct {
+	RepairMaterials []struct {
+		ShipRepairMaterialID string `json:"ShipRepairMaterialId"`
+		MaterialName         string `json:"MaterialName"`
+		MaterialID           string `json:"MaterialId"`
+		MaterialTicker       string `json:"MaterialTicker"`
+		Amount               int    `json:"Amount"`
+	} `json:"RepairMaterials"`
+	AddressLines []struct {
+		LineID    string `json:"LineId"`
+		LineType  string `json:"LineType"`
+		NaturalID string `json:"NaturalId"`
+		Name      string `json:"Name"`
+	} `json:"AddressLines"`
+	ShipID                   string    `json:"ShipId"`
+	StoreID                  string    `json:"StoreId"`
+	StlFuelStoreID           string    `json:"StlFuelStoreId"`
+	FtlFuelStoreID           string    `json:"FtlFuelStoreId"`
+	Registration             string    `json:"Registration"`
+	Name                     *string   `json:"Name"`
+	CommissioningTimeEpochMs int64     `json:"CommissioningTimeEpochMs"`
+	BlueprintNaturalID       string    `json:"BlueprintNaturalId"`
+	FlightID                 string    `json:"FlightId"`
+	Acceleration             float64   `json:"Acceleration"`
+	Thrust                   int       `json:"Thrust"`
+	Mass                     float64   `json:"Mass"`
+	OperatingEmptyMass       float64   `json:"OperatingEmptyMass"`
+	ReactorPower             int       `json:"ReactorPower"`
+	EmitterPower             int       `json:"EmitterPower"`
+	Volume                   int       `json:"Volume"`
+	Condition                float64   `json:"Condition"`
+	LastRepairEpochMs        int       `json:"LastRepairEpochMs"`
+	Location                 string    `json:"Location"`
+	StlFuelFlowRate          float64   `json:"StlFuelFlowRate"`
+	UserNameSubmitted        string    `json:"UserNameSubmitted"`
+	Timestamp                time.Time `json:"Timestamp"`
+}
+
+const endpoint = "/ship"
+
+func GetAll(ctx context.Context, username string, auth *fnar.Authentication) ([]*Ship, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fnar.BaseUrl+endpoint+"/ships/"+username, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating request for getting all ships")
+	}
+	req.Header.Add("Authorization", auth.AuthToken)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "getting all ships")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(string(b))
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "reading the response body")
+	}
+
+	// setup ship
+	shipsMap := []map[string]interface{}{}
+	if err := json.Unmarshal(body, &shipsMap); err != nil {
+		return nil, errors.Wrap(err, "decoding json response")
+	}
+
+	ships := []*Ship{}
+	for _, shipMap := range shipsMap {
+		ship := &Ship{}
+		if err := ship.parse(shipMap); err != nil {
+			return nil, errors.Wrap(err, "parse ship")
+		}
+		ships = append(ships, ship)
+	}
+
+	return ships, nil
+}
+
+func (s *Ship) parse(ship map[string]interface{}) error {
+	// correct timestamp to time.Time
+	var timestamp time.Time
+	var err error
+	ts, ok := ship["Timestamp"]
+	if ok {
+		timestamp, err = time.Parse("2006-01-02T15:04:05.999999", ts.(string))
+		if err != nil {
+			return err
+		}
+	}
+	ship["Timestamp"] = timestamp
+
+	// convert to Ship
+	shipM, err := json.Marshal(ship)
+	if err != nil {
+		return errors.Wrap(err, "marshaling ship map")
+	}
+	if err := json.Unmarshal(shipM, s); err != nil {
+		return errors.Wrap(err, "unmarshaling to Ship")
+	}
+
+	return nil
+}
